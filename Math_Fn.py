66a17d50a23b84a3218e3d588e384ce2bc352e13
neokd <71772185+neokd@users.noreply.github.com>
2020-10-06 19:13:23 +0530

    Add files via upload

---
+++ Math_Fn.py
@@ -0,0 +1,58 @@
+# MATH FUNCTIONS
+import math
+
+# rounds of the digit to the next integer
+print("ceil(4.4) : ", math.ceil(4.4))
+
+# Rounds of the number
+print("floor(4.4) : ", math.floor(4.4))
+
+# Absolute value for a float
+print("fabs(4.4) : ", math.fabs(4.4))
+
+# Factorial of a number 4 = 4 * 3 * 2 * 1
+print("Factorial(4) : ", math.factorial(4))
+
+# Remainder of a division
+print("fmod(4,2) : ", math.fmod(4,2))
+
+# truncate of a number
+print("trunc(4.4) : ", math.trunc(4.4))
+
+# power of a number x^y
+print("pow(2,2) : ", math.pow(2,2))
+print("2 ** 2 : ", 2 ** 2)
+
+# Square root of a number
+print("square root of a number is : ", math.sqrt(4))
+
+# value of pi
+print("pi : ", math.pi)
+
+# value of e
+print("e : ", math.e)
+
+#log base 10
+print("log(100,10) : ", math.log(100,10))
+
+# log10(100)
+print("log10(100) : ", math.log10(100))
+
+# ln = log base e
+print("ln(100) : ", math.log(100))
+
+# log base 2 in computer science
+print("log2(100) : ", math.log2(100))
+
+# Trigonometry fn
+# In Degrees
+print("sin(0) : ", math.sin(0))
+print("cos(0) : ", math.cos(0))
+print("tan(0) : ", math.tan(0))
+# In Radians
+print("sin(0) : ", math.asin(0))
+print("cos(0) : ", math.acos(0))
+print("tan(0) : ", math.atan(0))
+# Degrees & Radians
+print("degree(0) ", math.degrees(0))
+print("radians(0)", math.radians((0)))
